e6d257923f70d8753a4d3e450ad78d178ed4ae2d
Jazim-Abbas <jazimabbas87@gmail.com>
2021-11-29 13:22:41 +0500

    added form fields for country and year

--- src/App.js
+++ src/App.js
@@ -14,9 +14,39 @@ function App() {
   //   });
   // }, []);
 
+  const handleDateNavigate = (date) => {
+    const yearNo = date.getFullYear();
+    const monthNo = date.getMonth() + 1;
+    console.log("month navigate: ", monthNo, yearNo);
+  };
+
   return (
-    <div>
-      <p>Calender App</p>
+    <div className="container mb-3">
+      <div className="row my-5">
+        <div className="col-md-6">
+          <div className="form-group">
+            <label for="sel1">Select Year:</label>
+            <select className="form-control" id="sel1">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+            </select>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <div className="form-group">
+            <label for="sel1">Select Country:</label>
+            <select className="form-control" id="sel1">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+            </select>
+          </div>
+        </div>
+      </div>
+
       <Calendar
         localizer={localizer}
         events={events}
@@ -24,7 +54,8 @@ function App() {
         endAccessor="end"
         style={{ height: 500 }}
         views={["month"]}
-        onNavigate={(obj) => console.log("fired: ", obj)}
+        onNavigate={handleDateNavigate}
+        drilldownView="agenda"
       />
     </div>
   );
