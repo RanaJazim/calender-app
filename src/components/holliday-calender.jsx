cec8f43fd00c106ba60d11b3cf2b4caf20f2c629
Jazim-Abbas <jazimabbas87@gmail.com>
2021-11-29 18:59:02 +0500

    holliday-calender: remove unused code

--- src/components/holliday-calender.jsx
+++ src/components/holliday-calender.jsx
@@ -1,36 +1,18 @@
 import moment from "moment";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import { useSelector, useDispatch } from "react-redux";
-import { fetchYearlyHollidays } from "../apis/calender";
+import AppLoading from "./app-loading";
 import {
   loadHollidays,
   onYearChanged,
   currentDateChanged,
 } from "../store/holliday";
-import AppLoading from "./app-loading";
 
 const localizer = momentLocalizer(moment);
-// const events = [{ title: "Today", start: new Date(), end: new Date() }];
 
 export default function HollidayCalender() {
-  //   const [hollidayEvents, setHollidayEvents] = useState();
-
-  //   useEffect(() => {
-  //     fetchYearlyHollidays().then((res) => {
-  //       const hollidays = res.data.response.holidays;
-
-  //       const newHollidays = hollidays.map((h) => ({
-  //         title: h.name,
-  //         start: h.date.iso,
-  //         end: h.date.iso,
-  //       }));
-
-  //       setHollidayEvents(newHollidays);
-  //     });
-  //   }, []);
-
   const dispatch = useDispatch();
   const holliday = useSelector((state) => state.holliday);
   const currentDate = new Date(
@@ -64,7 +46,6 @@ export default function HollidayCalender() {
       style={{ height: 500 }}
       views={["month"]}
       onNavigate={handleDateNavigate}
-      drilldownView="agenda"
       date={currentDate}
     />
   );
