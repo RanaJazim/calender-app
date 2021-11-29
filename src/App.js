3341479e4b1b5573a7ea359aebb18cca0a3bf3f8
Jazim-Abbas <jazimabbas87@gmail.com>
2021-11-29 14:29:58 +0500

    extract yearlist to its own file

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import { Calendar, momentLocalizer } from "react-big-calendar";
 import { fetchAllCountries } from "./apis/calender";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import CountryList from "./components/country-list";
+import YearList from "./components/year-list";
 
 const localizer = momentLocalizer(moment);
 const events = [{ title: "Today", start: new Date(), end: new Date() }];
 
 function App() {
-  const [countries, setCountries] = useState();
 
-  useEffect(() => {
-    fetchAllCountries().then((res) => {
-      const countries = res.data.response.countries;
-      setCountries(countries);
-      console.log(countries);
-    });
-  }, []);
+  // useEffect(() => {
+  //   fetchAllCountries().then((res) => {
+  //     const countries = res.data.response.countries;
+  //     setCountries(countries);
+  //     console.log(countries);
+  //   });
+  // }, []);
 
   const handleDateNavigate = (date) => {
     const yearNo = date.getFullYear();
@@ -29,15 +29,7 @@ function App() {
     <div className="container mb-3">
       <div className="row my-5">
         <div className="col-md-6">
-          <div className="form-group">
-            <label>Select Year:</label>
-            <select className="form-control">
-              <option>1</option>
-              <option>2</option>
-              <option>3</option>
-              <option>4</option>
-            </select>
-          </div>
+         <YearList />
         </div>
         <div className="col-md-6">
           <CountryList />
---
+++ src/components/year-list.jsx
@@ -0,0 +1,13 @@
+export default function YearList() {
+  return (
+    <div className="form-group">
+      <label>Select Year:</label>
+      <select className="form-control">
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+      </select>
+    </div>
+  );
+}
