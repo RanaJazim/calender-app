298aa287f61294ff3b53de00633aa5df39bd0357
Jazim-Abbas <jazimabbas87@gmail.com>
2021-11-29 15:18:24 +0500

    fetching all hollidays for pakistan and display on the screen using calender

--- src/apis/calender.js
+++ src/apis/calender.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const API_KEY = "43f7a270aab91991f5eadc812d397f3ea9def7d7";
 
 export function fetchYearlyHollidays() {
-  const url = `https://calendarific.com/api/v2/holidays?&api_key=${API_KEY}&country=US&year=2019`;
+  const url = `https://calendarific.com/api/v2/holidays?&api_key=${API_KEY}&country=PK&year=2021`;
 
   return axios.get(url);
 }
--- src/components/holliday-calender.jsx
+++ src/components/holliday-calender.jsx
@@ -1,21 +1,44 @@
 import moment from "moment";
+import { useEffect, useState } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import { fetchYearlyHollidays } from "../apis/calender";
+import AppLoading from "./app-loading";
 
 const localizer = momentLocalizer(moment);
-const events = [{ title: "Today", start: new Date(), end: new Date() }];
+// const events = [{ title: "Today", start: new Date(), end: new Date() }];
 
 export default function HollidayCalender() {
+  const [hollidayEvents, setHollidayEvents] = useState();
+
+  useEffect(() => {
+    fetchYearlyHollidays().then((res) => {
+      const hollidays = res.data.response.holidays;
+
+      const newHollidays = hollidays.map((h) => ({
+        title: h.name,
+        start: h.date.iso,
+        end: h.date.iso,
+      }));
+
+      setHollidayEvents(newHollidays);
+    });
+  }, []);
+
   const handleDateNavigate = (date) => {
     const yearNo = date.getFullYear();
     const monthNo = date.getMonth() + 1;
     console.log("month navigate: ", monthNo, yearNo);
   };
 
+  if (!hollidayEvents) {
+    return <AppLoading />;
+  }
+
   return (
     <Calendar
       localizer={localizer}
-      events={events}
+      events={hollidayEvents}
       startAccessor="start"
       endAccessor="end"
       style={{ height: 500 }}
