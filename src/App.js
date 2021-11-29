198831d5b5b934f0e42c6716a1a98d5f097136a8
Jazim-Abbas <jazimabbas87@gmail.com>
2021-11-29 13:06:37 +0500

    displaying calender

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
+import moment from "moment";
 import { useEffect } from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
 import { fetchYearlyHollidays } from "./apis/calender";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const localizer = momentLocalizer(moment);
+const events = [{ title: "Today", start: new Date(), end: new Date() }];
 
 function App() {
-  useEffect(() => {
-    fetchYearlyHollidays().then((res) => {
-      console.log(res.data.response.holidays);
-    });
-  }, []);
+  // useEffect(() => {
+  //   fetchYearlyHollidays().then((res) => {
+  //     console.log(res.data.response.holidays);
+  //   });
+  // }, []);
 
   return (
     <div>
       <p>Calender App</p>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+        views={["month"]}
+        onNavigate={(obj) => console.log("fired: ", obj)}
+      />
     </div>
   );
 }
